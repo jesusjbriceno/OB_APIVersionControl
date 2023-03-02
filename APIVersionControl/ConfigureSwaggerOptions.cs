63465a24ff0f0b9ec52b93ee0c4fab35cce48f6a
Jesús Briceño <ineco.jjbm@externomf.es>
2023-03-02 19:00:21 +0100

    Proyecto inicial

---
+++ APIVersionControl/ConfigureSwaggerOptions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace APIVersionControl
+{
+    public class ConfigureSwaggerOptions : IConfigureNamedOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => _provider = provider;
+
+        private OpenApiInfo CreateVersionInfo(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo()
+            {
+                Title = "My .Net Api restful",
+                Version = description.ApiVersion.ToString(),
+                Description = $"This is my first API versioning control.",
+                Contact = new OpenApiContact()
+                {
+                    Email = "jesusj.briceno@gmail.com",
+                    Name = "Jesús"
+                }
+            };
+
+            if(description.IsDeprecated)
+            {
+                info.Description += "This version is deprecated";
+            }
+
+            return info;
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            // Add Swagger Documentation for each API version we have
+            foreach(var description in _provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateVersionInfo(description));
+            }
+        }
+
+        public void Configure(string name, SwaggerGenOptions options) => Configure(options);
+    }
+}
---
+++ APIVersionControl/Controllers/v2/UsersControllerV2.cs
@@ -0,0 +1,35 @@
+﻿using APIVersionControl.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace APIVersionControl.Controllers.v2
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private const string API_TEST_URL = "https://dummyapi.io/data/v1/user?limit=30";
+        private const string API_TEST_ID = "6400d8c475ef7d3e3d8e4100";
+        private readonly HttpClient _httpClient;
+
+        public UsersController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [MapToApiVersion("2.0")]
+        [HttpGet(Name = "GetUsersData")]
+        public async Task<IActionResult> GetUsersDataAsync()
+        {
+            _httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.Add("app-id", API_TEST_ID);
+
+            var response = await _httpClient.GetStreamAsync(API_TEST_URL);
+            var usersData = await JsonSerializer.DeserializeAsync<UsersResponseData>(response);
+
+            return Ok(usersData?.data);
+        }
+    }
+}
